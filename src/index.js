e8f303642c09ccca101a193d3e2473939ad27348
Dominik Biel <nes1kd@gmail.com>
2016-08-24 17:46:46 +0200

    added redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import { Game } from './containers';
+import reducers from './reducers';
+
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-    <div>Hello</div>,
+  <Provider store={store}>
+    <Game />
+  </Provider>,
   document.querySelector('#root')
 );
